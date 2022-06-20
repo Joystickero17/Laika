975318493343402e8a4e6ce61fc55a2e23cdff44
Augusto Carrillo <augustocarrillo20@gmail.com>
2022-06-19 22:49:21 -0400

    working in modal error component

---
+++ src/components/ErrorModal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Se presentaron los siguientes errores
+    </div>
+</template>
+
+<script>
+export default {
+data: ()=>{
+    return {
+
+    }
+},
+props:[
+    "errorList"
+]
+}
+</script>
+
+<style>
+
+</style>
